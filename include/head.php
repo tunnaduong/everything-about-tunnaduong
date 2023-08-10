f0bffa7ecc31eba82ab5f452b1e19e811e2470cf
Tunna Duong <tunnaduong@gmail.com>
2023-08-10 17:28:33 +0700

    Changed index file type to php

---
+++ include/head.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="icon" type="image/x-png" href="/static/img/tunnaduong.png" />
+<title>Tất cả mọi thứ về Tunna Duong</title>
+<link rel="stylesheet" href="/static/css/style.css" />
+<script src="/static/js/jquery.min.js"></script>
+<script src="/static/js/typed.min.js"></script>
