edd4628b3f0fc518e9f5b9e5dcd159e073056279
Tunna Duong <tunnaduong@gmail.com>
2023-08-11 12:26:57 +0700

    Changed path of everyday diary

--- index.php
+++ index.php
@@ -2,22 +2,22 @@
 <html lang="en">
 
 <head>
-    <?php
+  <?php
   require_once $_SERVER['DOCUMENT_ROOT'] . '/serverconnect.php';
   require_once $_SERVER['DOCUMENT_ROOT'] . '/include/head.php';
   ?>
 </head>
 
 <body>
-    <!-- Construction badge: <img src="./static/img/under-construction.png" class="under-construction-badge" /> -->
-    <!-- <img
+  <!-- Construction badge: <img src="./static/img/under-construction.png" class="under-construction-badge" /> -->
+  <!-- <img
       src="./static/img/under-construction.png"
       class="under-construction-badge"
       alt="Under construction badge"
       title="Bạn muốn góp ý cho website? Bấm vào ảnh nhé..."
     /> -->
-    <!-- Popup alert showing Figma design -->
-    <!-- <div id="floating-footer-alert">
+  <!-- Popup alert showing Figma design -->
+  <!-- <div id="floating-footer-alert">
       <span id="alert-close-btn">&times;</span>
       <h3>👋 Xin chào đằng ấy!</h3>
       <p>
@@ -37,18 +37,18 @@
         nhé! ❤️
       </p>
     </div> -->
-    <?php
+  <?php
   require_once $_SERVER['DOCUMENT_ROOT'] . '/include/navbar.php';
   ?>
-    <div class="main">
-        <div class="main--section-1">
-            <h1>
-                <a href="/everyday" class="no-color">
-                    Nhật ký<br />hằng ngày
-                </a>
-            </h1>
-            <div class="section--content">
-                <?php
+  <div class="main">
+    <div class="main--section-1">
+      <h1>
+        <a href="/nhat-ky-hang-ngay" class="no-color">
+          Nhật ký<br />hằng ngày
+        </a>
+      </h1>
+      <div class="section--content">
+        <?php
         mysqli_select_db($db2, 'tunnaduong_everyday');
         $sql = "SELECT * FROM timeline ORDER BY id DESC LIMIT 3";
         $result = $db2->query($sql);
@@ -58,35 +58,35 @@
           while ($row = $result->fetch_assoc()) {
         ?>
 
-                <div class="content--card">
-                    <a class=" no-color" href="https://everyday.tunnaduong.com" target="_blank">
-                        <p id="everyday-date-<?php echo $row['id'] ?>" class="card--date">
-                            <?php echo $row['date']; ?>
-                        </p>
-                        <p class="card--summary"><?php echo $row['title']; ?></p>
-                    </a>
-                </div>
-                <script>
-                $("#everyday-date-<?php echo $row['id']; ?>").text(moment('<?php echo $row['date']; ?>').format(
-                    'DD/MM/YYYY'));
-                </script>
-                <?php
+            <div class="content--card">
+              <a class=" no-color" href="https://everyday.tunnaduong.com" target="_blank">
+                <p id="everyday-date-<?php echo $row['id'] ?>" class="card--date">
+                  <?php echo $row['date']; ?>
+                </p>
+                <p class="card--summary"><?php echo $row['title']; ?></p>
+              </a>
+            </div>
+            <script>
+              $("#everyday-date-<?php echo $row['id']; ?>").text(moment('<?php echo $row['date']; ?>').format(
+                'DD/MM/YYYY'));
+            </script>
+        <?php
           }
         } else {
           echo "0 kết quả";
         }
         $db2->close();
         ?>
-            </div>
-        </div>
-        <div class="main--section-2">
-            <h1>
-                <a href="https://blog.tunnaduong.com" class="no-color">
-                    Bài viết<br />blog
-                </a>
-            </h1>
-            <div class="section--content">
-                <?php
+      </div>
+    </div>
+    <div class="main--section-2">
+      <h1>
+        <a href="https://blog.tunnaduong.com" class="no-color">
+          Bài viết<br />blog
+        </a>
+      </h1>
+      <div class="section--content">
+        <?php
         mysqli_select_db($db3, 'tunnaduong_blog');
         $sql = "SELECT * FROM wp_posts WHERE post_type = 'post' AND post_name != 'quote' AND post_status != 'draft' ORDER BY id DESC LIMIT 3";
         $result = $db3->query($sql);
@@ -96,167 +96,166 @@
           while ($row = $result->fetch_assoc()) {
         ?>
 
-                <div class="content--card"">
+            <div class="content--card"">
                 <a class=" no-color" href=" <?php echo $row['guid'] ?>" target="_blank">
-                    <p id="blog-date-<?php echo $row['ID'] ?>" class="card--date">
-                        <?php echo $row['post_date']; ?>
-                    </p>
-                    <p class="card--summary"><?php echo $row['post_title']; ?></p>
-                    </a>
-                </div>
-                <script>
-                $("#blog-date-<?php echo $row['ID']; ?>").text(moment('<?php echo $row['post_date']; ?>').format(
-                    'DD/MM/YYYY'));
-                </script>
-                <?php
+              <p id="blog-date-<?php echo $row['ID'] ?>" class="card--date">
+                <?php echo $row['post_date']; ?>
+              </p>
+              <p class="card--summary"><?php echo $row['post_title']; ?></p>
+              </a>
+            </div>
+            <script>
+              $("#blog-date-<?php echo $row['ID']; ?>").text(moment('<?php echo $row['post_date']; ?>').format(
+                'DD/MM/YYYY'));
+            </script>
+        <?php
           }
         } else {
           echo "0 kết quả";
         }
         $db3->close();
         ?>
-            </div>
-        </div>
-        <div class="main--section-3">
-            <h1>Những người<br />đã gặp</h1>
-            <div class="section--content people-i-met">
-                <div class="people-thumb">
-                    <img src="./static/img/people/linhngo.jpg" alt="Ngô Nguyễn Thảo Linh" />
-                    <div class="people-details">
-                        <img src="./static/img/people/linhngo.jpg" alt="Ngô Nguyễn Thảo Linh" />
-                        <div class="details">
-                            <p class="details--name">Ngô Nguyễn Thảo Linh</p>
-                            <p class="details--summary">3 kỉ niệm cùng nhau</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="people-thumb">
-                    <img src="./static/img/people/thanhcong.png" alt="Phạm Thành Công" />
-                    <div class="people-details">
-                        <img src="./static/img/people/thanhcong.png" alt="Phạm Thành Công" />
-                        <div class="details">
-                            <p class="details--name">Ngô Nguyễn Thảo Linh</p>
-                            <p class="details--summary">3 kỉ niệm cùng nhau</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="people-thumb">
-                    <img src="./static/img/people/phanducmanh.png" alt="Phan Đức Mạnh" />
-                    <div class="people-details">
-                        <img src="./static/img/people/phanducmanh.png" alt="Phan Đức Mạnh" />
-                        <div class="details">
-                            <p class="details--name">Ngô Nguyễn Thảo Linh</p>
-                            <p class="details--summary">3 kỉ niệm cùng nhau</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="people-thumb">
-                    <img src="./static/img/people/luongquangthang.png" alt="Lương Quang Thắng" />
-                    <div class="people-details">
-                        <img src="./static/img/people/luongquangthang.png" alt="Lương Quang Thắng" />
-                        <div class="details">
-                            <p class="details--name">Ngô Nguyễn Thảo Linh</p>
-                            <p class="details--summary">3 kỉ niệm cùng nhau</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="people-thumb">
-                    <img src="./static/img/people/hongquan.png" alt="Đỗ Hồng Quân" />
-                    <div class="people-details">
-                        <img src="./static/img/people/hongquan.png" alt="Đỗ Hồng Quân" />
-                        <div class="details">
-                            <p class="details--name">Ngô Nguyễn Thảo Linh</p>
-                            <p class="details--summary">3 kỉ niệm cùng nhau</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="people-thumb">
-                    <img src="./static/img/people/haquangthang.png" alt="Hà Quang Thắng" />
-                    <div class="people-details">
-                        <img src="./static/img/people/haquangthang.png" alt="Hà Quang Thắng" />
-                        <div class="details">
-                            <p class="details--name">Ngô Nguyễn Thảo Linh</p>
-                            <p class="details--summary">3 kỉ niệm cùng nhau</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="people-thumb">
-                    <img src="./static/img/people/lelam.png" alt="Lê Hoàng Tùng Lâm" />
-                    <div class="people-details">
-                        <img src="./static/img/people/lelam.png" alt="Lê Hoàng Tùng Lâm" />
-                        <div class="details">
-                            <p class="details--name">Ngô Nguyễn Thảo Linh</p>
-                            <p class="details--summary">3 kỉ niệm cùng nhau</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="people-thumb">
-                    <img src="./static/img/people/thienhuong.png" alt="Phạm Thị Thiên Hương" />
-                    <div class="people-details">
-                        <img src="./static/img/people/thienhuong.png" alt="Phạm Thị Thiên Hương" />
-                        <div class="details">
-                            <p class="details--name">Ngô Nguyễn Thảo Linh</p>
-                            <p class="details--summary">3 kỉ niệm cùng nhau</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="people-thumb">
-                    <img src="./static//img/people/buihien.png" alt="Bùi Thu Hiền" />
-                    <div class="people-details">
-                        <img src="./static//img/people/buihien.png" alt="Bùi Thu Hiền" />
-                        <div class="details">
-                            <p class="details--name">Ngô Nguyễn Thảo Linh</p>
-                            <p class="details--summary">3 kỉ niệm cùng nhau</p>
-                        </div>
-                    </div>
-                </div>
-                <div id="additional-number">+26</div>
-            </div>
-        </div>
-        <div class="main--section-4">
-            <h1>Mọi người nghĩ<br />gì về mình</h1>
-            <div class="section--content">
-                <div class="content--card card--people">
-                    <img src="./static/img/people/lelam.png" alt="Lê Hoàng Tùng Lâm" />
-                    <div>
-                        <p class="people--name">Lê Hoàng Tùng Lâm</p>
-                        <p class="people--review">Bạn ơi đừng béo nữa</p>
-                    </div>
-                </div>
-                <div class="content--card card--people">
-                    <img src="./static/img/people/thienhuong.png" alt="Phạm Thị Thiên Hương" />
-                    <div>
-                        <p class="people--name">Hương xinh</p>
-                        <p class="people--review">Giảm cân đi cậu nhé!</p>
-                    </div>
-                </div>
-                <div class="content--card card--people">
-                    <img src="./static/img/people/buihien.png" alt="Bùi Thu Hiền" />
-                    <div>
-                        <p class="people--name">Bùi Thu Hiền</p>
-                        <p class="people--review">
-                            Chúc đỗ nguyện vọng mong ước, luôn dui dẻ, hạnh phúc với những
-                            quyết định của mình nho
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-    <center>
-        <footer>
-            <p class="footer--copyright">
-                <span id="footer--mobile">© 2022-<?php echo date("Y") ?> Duong Tung Anh<br /><span
-                        style="color: gray; font-weight: 300; font-size: 15px">All rights reserved</span></span>
-                <span id="footer--desktop">© 2022-<?php echo date("Y") ?> Duong Tung Anh. All rights reserved.</span>
+    <div class="main--section-3">
+      <h1>Những người<br />đã gặp</h1>
+      <div class="section--content people-i-met">
+        <div class="people-thumb">
+          <img src="./static/img/people/linhngo.jpg" alt="Ngô Nguyễn Thảo Linh" />
+          <div class="people-details">
+            <img src="./static/img/people/linhngo.jpg" alt="Ngô Nguyễn Thảo Linh" />
+            <div class="details">
+              <p class="details--name">Ngô Nguyễn Thảo Linh</p>
+              <p class="details--summary">3 kỉ niệm cùng nhau</p>
+            </div>
+          </div>
+        </div>
+        <div class="people-thumb">
+          <img src="./static/img/people/thanhcong.png" alt="Phạm Thành Công" />
+          <div class="people-details">
+            <img src="./static/img/people/thanhcong.png" alt="Phạm Thành Công" />
+            <div class="details">
+              <p class="details--name">Ngô Nguyễn Thảo Linh</p>
+              <p class="details--summary">3 kỉ niệm cùng nhau</p>
+            </div>
+          </div>
+        </div>
+        <div class="people-thumb">
+          <img src="./static/img/people/phanducmanh.png" alt="Phan Đức Mạnh" />
+          <div class="people-details">
+            <img src="./static/img/people/phanducmanh.png" alt="Phan Đức Mạnh" />
+            <div class="details">
+              <p class="details--name">Ngô Nguyễn Thảo Linh</p>
+              <p class="details--summary">3 kỉ niệm cùng nhau</p>
+            </div>
+          </div>
+        </div>
+        <div class="people-thumb">
+          <img src="./static/img/people/luongquangthang.png" alt="Lương Quang Thắng" />
+          <div class="people-details">
+            <img src="./static/img/people/luongquangthang.png" alt="Lương Quang Thắng" />
+            <div class="details">
+              <p class="details--name">Ngô Nguyễn Thảo Linh</p>
+              <p class="details--summary">3 kỉ niệm cùng nhau</p>
+            </div>
+          </div>
+        </div>
+        <div class="people-thumb">
+          <img src="./static/img/people/hongquan.png" alt="Đỗ Hồng Quân" />
+          <div class="people-details">
+            <img src="./static/img/people/hongquan.png" alt="Đỗ Hồng Quân" />
+            <div class="details">
+              <p class="details--name">Ngô Nguyễn Thảo Linh</p>
+              <p class="details--summary">3 kỉ niệm cùng nhau</p>
+            </div>
+          </div>
+        </div>
+        <div class="people-thumb">
+          <img src="./static/img/people/haquangthang.png" alt="Hà Quang Thắng" />
+          <div class="people-details">
+            <img src="./static/img/people/haquangthang.png" alt="Hà Quang Thắng" />
+            <div class="details">
+              <p class="details--name">Ngô Nguyễn Thảo Linh</p>
+              <p class="details--summary">3 kỉ niệm cùng nhau</p>
+            </div>
+          </div>
+        </div>
+        <div class="people-thumb">
+          <img src="./static/img/people/lelam.png" alt="Lê Hoàng Tùng Lâm" />
+          <div class="people-details">
+            <img src="./static/img/people/lelam.png" alt="Lê Hoàng Tùng Lâm" />
+            <div class="details">
+              <p class="details--name">Ngô Nguyễn Thảo Linh</p>
+              <p class="details--summary">3 kỉ niệm cùng nhau</p>
+            </div>
+          </div>
+        </div>
+        <div class="people-thumb">
+          <img src="./static/img/people/thienhuong.png" alt="Phạm Thị Thiên Hương" />
+          <div class="people-details">
+            <img src="./static/img/people/thienhuong.png" alt="Phạm Thị Thiên Hương" />
+            <div class="details">
+              <p class="details--name">Ngô Nguyễn Thảo Linh</p>
+              <p class="details--summary">3 kỉ niệm cùng nhau</p>
+            </div>
+          </div>
+        </div>
+        <div class="people-thumb">
+          <img src="./static//img/people/buihien.png" alt="Bùi Thu Hiền" />
+          <div class="people-details">
+            <img src="./static//img/people/buihien.png" alt="Bùi Thu Hiền" />
+            <div class="details">
+              <p class="details--name">Ngô Nguyễn Thảo Linh</p>
+              <p class="details--summary">3 kỉ niệm cùng nhau</p>
+            </div>
+          </div>
+        </div>
+        <div id="additional-number">+26</div>
+      </div>
+    </div>
+    <div class="main--section-4">
+      <h1>Mọi người nghĩ<br />gì về mình</h1>
+      <div class="section--content">
+        <div class="content--card card--people">
+          <img src="./static/img/people/lelam.png" alt="Lê Hoàng Tùng Lâm" />
+          <div>
+            <p class="people--name">Lê Hoàng Tùng Lâm</p>
+            <p class="people--review">Bạn ơi đừng béo nữa</p>
+          </div>
+        </div>
+        <div class="content--card card--people">
+          <img src="./static/img/people/thienhuong.png" alt="Phạm Thị Thiên Hương" />
+          <div>
+            <p class="people--name">Hương xinh</p>
+            <p class="people--review">Giảm cân đi cậu nhé!</p>
+          </div>
+        </div>
+        <div class="content--card card--people">
+          <img src="./static/img/people/buihien.png" alt="Bùi Thu Hiền" />
+          <div>
+            <p class="people--name">Bùi Thu Hiền</p>
+            <p class="people--review">
+              Chúc đỗ nguyện vọng mong ước, luôn dui dẻ, hạnh phúc với những
+              quyết định của mình nho
             </p>
-            <p class="footer--fun">
-                Được làm bằng 💕 <i>tình yêu</i>, 🔥 <i>nhiệt huyết</i>, ⌨️
-                <i>bàn phím</i> và rất nhiều ☕️ <i>cà phê</i>.
-            </p>
-        </footer>
-    </center>
-    <script src="/static/js/script.js"></script>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <center>
+    <footer>
+      <p class="footer--copyright">
+        <span id="footer--mobile">© 2022-<?php echo date("Y") ?> Duong Tung Anh<br /><span style="color: gray; font-weight: 300; font-size: 15px">All rights reserved</span></span>
+        <span id="footer--desktop">© 2022-<?php echo date("Y") ?> Duong Tung Anh. All rights reserved.</span>
+      </p>
+      <p class="footer--fun">
+        Được làm bằng 💕 <i>tình yêu</i>, 🔥 <i>nhiệt huyết</i>, ⌨️
+        <i>bàn phím</i> và rất nhiều ☕️ <i>cà phê</i>.
+      </p>
+    </footer>
+  </center>
+  <script src="/static/js/script.js"></script>
 </body>
 
 </html>
--- nhat-ky-hang-ngay/index.php
+++ nhat-ky-hang-ngay/index.php
@@ -37,7 +37,16 @@
                         <p class="card--title">
                             Để không bị lãng quên
                         </p>
-                        <p class="card--summary">Lorem ipsum isdjaspidsapjoidoipjasdpoiasjdpo</p>
+                        <p class="card--content">Bài viết thứ hai trong ngày hôm nay, đó là về ý tưởng mới của mình. Đại
+                            khái là mình sẽ tạo một trang web đơn giản lưu trữ các bài viết tự học và ôn luyện để ôn thi
+                            Final Exam cho sinh viên đang học ngành Kỹ thuật phần mềm tại Đại học FPT. Về bản chất thì
+                            nó cũng giống các web khác của các anh chị khóa trên như kiểu Thaycacac, Kungfutech,...
+                            nhưng mà ngoài chia sẻ những tài liệu ôn luyện, key, đề tự ôn, mình sẽ chia sẻ về quá trình
+                            tự ôn luyện để lấy gốc thi Final Exam các môn trong kì mà mình đang học. Trong bối cảnh mà
+                            chỉ còn 2 ngày nữa là mình bắt đầu thi PE và hôm sau thi FE môn toán kỹ thuật :))). Web này
+                            đang được phát triển tại địa chỉ: https://naominhcungon.github.io. Bao giờ có tiền và
+                            website được nhiều người biết đến thì mình sẽ chuyển sang địa chỉ mới có tên miền ngắn gọn
+                            hơn :)))</p>
                     </a>
                 </div>
             </div>
