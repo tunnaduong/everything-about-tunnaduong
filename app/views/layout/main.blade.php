99ca8a7047e415b07c0711e27ae99355e09f341f
Dương Tùng Anh <tunnaduong@gmail.com>
2025-04-03 12:17:06 +0700

    ok

--- app/views/layout/main.blade.php
+++ app/views/layout/main.blade.php
@@ -1,36 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
+l lang="en">
 @php
     $rel = isset($_GET['rel']) ? $_GET['rel'] : null;
 @endphp
-@if ($rel != 'page')
-
-    <head>
-        <!-- Google tag (gtag.js) -->
+@i f ($rel != 'page'
+    )
+    
+        <head>
+            <!-- Google tag (gtag.js) -->
+    
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-8KH78J0G18"></script>
         <script>
-            window.dataLayer = window.dataLayer || [];
+      
+      window.dataLayer = window.dataLayer || [];
 
             function gtag() {
-                dataLayer.push(arguments);
+                 dat
+    aLayer.push(arguments);
             }
             gtag('js', new Date());
+
 
             gtag('config', 'G-8KH78J0G18');
         </script>
         <meta charset="UTF-8" />
+
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" type="image/x-png" href="/static/img/tunnaduong.png" />
-        <title>@yield('title', 'Tất cả mọi thứ về Tunna Duong')</title>
-        <link rel="stylesheet" href="/static/css/style.css" />
-        <script src="/static/js/jquery.min.js"></script>
-        <script src="/static/js/typed.min.js"></script>
-        <script src="/static/js/moment.min.js"></script>
-        <script src="/static/js/nprogress.js"></script>
+         <link rel="
+    icon" type="image/x-png" href="/static/img/tunnaduong.png" />
+            <title>@yield('title', 'Tất cả mọi thứ về Tunna Duong')</title>
+            <link rel="stylesheet" href="/static/css/style.css" />
+           <script rc="/static/js/jquery.min.js"></script>
+            <script src="/static/js/typed.min.js"></script>
+            <script src="/static/js/moment.min.js"></script>
+           <script rc="/static/js/nprogress.js"></script>
         <link rel='stylesheet' href='/static/css/nprogress.css' />
         <script src="https://kit.fontawesome.com/be3d8625b2.js" crossorigin="anonymous"></script>
-        <script src="https://cdn.tailwindcss.com"></script>
+      
+  <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script>
         <script src="https://challenges.cloudflare.com/turnstile/v0/api.js?onload=onloadTurnstileCallback" defer></script>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3425905751761094"
@@ -41,33 +49,33 @@
 <body onclick="">
     @if ($rel != 'page')
     <!-- Construction badge: <img src="./static/img/under-construction.png" class="under-construction-badge" /> -->
-  <img
-      src="/static/img/under-construction.png"
-      class="under-construction-badge"
-      alt="Under construction badge"
-      title="Bạn muốn góp ý cho website? Bấm vào ảnh nhé..."
-    />
-  <!-- Popup alert showing Figma design -->
-  <div id="floating-footer-alert">
-      <span id="alert-close-btn">&times;</span>
-      <h3>👋 Xin chào đằng ấy!</h3>
-      <p>
-        Trang web hiện tại vẫn đang được phát triển và hoàn thiện. Tuy nhiên bạn
-        cũng có thể xem qua bản thiết kế của trang web này tại:
-        <a
-          href="https://www.figma.com/file/cHrOJ4ASoJUWielefafzzC/Life-Of-Tunna"
-          target="_blank"
-          >Figma</a
-        >
-        để dễ hình dung. Và nếu có thể thì cho mình xin một feedback qua trang
-        <a
-          href="https://github.com/tunnaduong/everything-about-tunnaduong/issues"
-          target="_blank"
-          >Google Forms</a
-        >
-        nho nhỏ này nhé! ❤️
-      </p>
-    </div>
+        <img
+        src="/static/img/under-construction.png"
+        class="under-construction-badge"
+        alt="Under construction badge"
+        title="Bạn muốn góp ý cho website? Bấm vào ảnh nhé..."
+        />
+        <!-- Popup alert showing Figma design -->
+        <div id="floating-footer-alert">
+            <span id="alert-close-btn">&times;</span>
+            <h3>👋 Xin chào đằng ấy!</h3>
+            <p>
+                Trang web hiện tại vẫn đang được phát triển và hoàn thiện. Tuy nhiên bạn
+                cũng có thể xem qua bản thiết kế của trang web này tại:
+                <a
+                href="https://www.figma.com/file/cHrOJ4ASoJUWielefafzzC/Life-Of-Tunna"
+                target="_blank"
+                >Figma</a
+                >
+                để dễ hình dung. Và nếu có thể thì cho mình xin một feedback qua trang
+                <a
+                href="https://forms.gle/BduoqJsY6FLxKzxv7"
+                target="_blank"
+                >Google Forms</a
+                >
+                nho nhỏ này nhé! ❤️
+            </p>
+        </div>
         <center>
             <div id="header" href="/" hide-nav style="cursor: pointer; width: 400px;">
                 <img src="/static/img/tunnaduong.png" alt="Tunna Duong logo" class="logo" />
